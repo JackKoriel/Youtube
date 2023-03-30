92014e80540119e0a891ac7d3e02829ec0917c75
Jack Koriel <jkoriel@gmail.com>
2023-03-30 12:02:40 -0400

    fix: typo

--- src/utility/Theme.js
+++ src/utility/Theme.js
@@ -2,7 +2,7 @@ export const darkTheme = {
   bg: "#181818",
   bgLight: "#202020",
   text: "white",
-  textSoft: "aaaaaa",
+  textSoft: "#aaaaaa",
   soft: "#373737",
 };
 
@@ -10,6 +10,6 @@ export const lightTheme = {
   bg: "#f9f9f9",
   bgLight: "white",
   text: "black",
-  textSoft: "606060",
+  textSoft: "#606060",
   soft: "#f5f5f5",
 };
