4d3200ee82e515ee377fd3d0df4ceb94a0acc783
Jack Koriel <jkoriel@gmail.com>
2023-04-01 22:18:50 -0400

    feat: creates test routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
 
 const app = express();
 dotenv.config();
@@ -15,6 +18,10 @@ const connect = () => {
       throw err;
     });
 };
+
+app.use("/api/users", userRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/videos", videoRoutes);
 
 app.listen(8800, () => {
   connect();
