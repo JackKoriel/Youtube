03dd2f6fb9ceb5f4f4924bd2567cb25e46b26fa1
Jack Koriel <jkoriel@gmail.com>
2023-04-02 22:06:10 -0400

    feat: creates basic user routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,13 @@
-export const test = (req, res) => {
-  res.json("Its successful");
-};
+export const update = (req, res, next) => {};
+
+export const deleteUser = (req, res, next) => {};
+
+export const getUser = (req, res, next) => {};
+
+export const subscribe = (req, res, next) => {};
+
+export const unsubscribe = (req, res, next) => {};
+
+export const like = (req, res, next) => {};
+
+export const dislike = (req, res, next) => {};
