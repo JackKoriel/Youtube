982fc722aa6e846f4b0ed39a4b809e6752dea0a7
Jack Koriel <jkoriel@gmail.com>
2023-04-05 21:14:00 -0400

    feat: adds comments

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -18,22 +18,31 @@ const router = express.Router();
 //create a video
 router.post("/", verifyToken, addVideo);
 
+//update a video
 router.put("/:id", verifyToken, updateVideo);
 
+//increment views
 router.put("/view/:id", addView);
 
+//delete a video
 router.delete("/:id", verifyToken, deleteVideo);
 
+//find a video
 router.get("/find/:id", getVideo);
 
+//show trendy videos
 router.get("/trend", trend);
 
+//get random videos
 router.get("/random", random);
 
+//find videos from channels subscribed to
 router.get("/sub", verifyToken, sub);
 
+//find a by tag video
 router.get("/tags", getByTag);
 
+//find a video with search
 router.get("/search", search);
 
 export default router;
